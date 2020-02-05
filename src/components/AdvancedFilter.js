d611db99792a9261b578eec8d8c5f0951e906128
Eugenia Kim <elk303@nyu.edu>
2020-02-05 15:09:23 -0500

    adv filter handle click comments

--- src/components/AdvancedFilter.js
+++ src/components/AdvancedFilter.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 class AdvancedFilter extends React.Component {
 
+  handleClick = () => {
+    //This event handler would go to another page using react-router (which still has to be implemented)
+    
+  }
+
   render(){
     return (
       <div className="advanced-search-box">
         <h2 className="adv-search-header"> Advanced Search  </h2> 
-        <button className="adv-search-btn"> Click Here </button>
+        <button className="adv-search-btn" onClick={this.handleClick}> Click Here </button> 
       </div>
     )
   }
